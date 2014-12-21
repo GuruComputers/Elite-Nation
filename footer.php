ef26509bef72e511c0520801d32d7087993348d6
Rev Neil Wakely <neil@gurucomputers.co.uk>
2014-12-21 19:31:37 +0000

    fb & twitter links

--- footer.php
+++ footer.php
@@ -13,7 +13,7 @@
 			}
 			echo "&copy"." ".$cy." <a href=\"http://www.gurucomputers.co.uk\">Guru Computers Ltd</a>"."\n";
 		?>
-		<img class="social-media" src="./img/facebook.png" alt="Facebook Logo">
-		<img class="social-media" src="./img/twitter.png" alt="Twitter Logo">		
+		<a href="https://www.facebook.com/elitenationgame"><img class="social-media" src="./img/facebook.png" alt="Facebook Logo"></a>
+		<a href="https://twitter.com/EliteNationGame"><img class="social-media" src="./img/twitter.png" alt="Twitter Logo"></a>	
 	</center>
 </footer>
