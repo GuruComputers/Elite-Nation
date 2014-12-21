af92e47ef2e5076116d8d17ef0b61cd3fd8fa436
Rev Dave Wakely <dave@gurucomputers.co.uk>
2014-12-21 17:13:24 +0000

    Adjustments to make game Mobile Friendly

--- index.php
+++ index.php
@@ -25,17 +25,25 @@ if(!$_SESSION['login_user']==""){
 <body>	
 	<div id="wrapper">
 		<header>
+		<div id="mobile-logo">
 			<img class="logo" src="img/logo.png" alt="Elite Nation Logo" />
+		</div>
+			<br class="anti-oops" />
 			<form id="login" action="" method="post">
-				<label>UserName :</label>
-				<input id="name" name="username" placeholder="username" type="text">
-				<label>Password :</label>
-				<input id="password" name="password" placeholder="**********" type="password">
-				<input name="submit" type="submit" value=" Login ">
-				<br /><br />
-				<span class="user-tools">
-					<a href="register.php">Register Now</a>          <a href="forgot.php">Forgotten Password?</a>
-				</span>
+				<div id="mobile-user">
+					<label>User Name :</label>
+					<input id="name" name="username" placeholder="username" type="text">
+				</div>
+				<div id="mobile-password">
+					<label>Password :</label>
+					<input id="password" name="password" placeholder="**********" type="password">
+				</div>
+				<input name="submit" type="submit" value=" Login " id="submit_button">
+				<br />
+				<ul class="user-tools">
+					<li><a href="register.php">Register Now</a></li>
+					<li><a href="forgot.php">Forgotten Password?</a></li>
+				</ul>
 			</form>
 		</header>
 		<section>
