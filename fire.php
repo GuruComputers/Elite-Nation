eaeb9859bf680ab4d1b800a623867a85be6dcf73
Rev Neil Wakely <neil@gurucomputers.co.uk>
2014-12-21 17:53:35 +0000

    no (c) on fire

--- fire.php
+++ fire.php
@@ -21,28 +21,13 @@
 		</div>
 			<br/><br/>
 			<h1>Sorry your mobile is currently not supported</h1>
+			<br/><br/>
 		</header>
 		<section>
 				
 		</section>
 		<footer>
-			<center>
-				<?php
-					$start = "2013";
-					$current = gmdate("Y");
-					$cy = "";
-					if ($current == $start) {
-						$cy = $start;
-					}
-
-					if ($current > $start) {
-						$cy=$start."-".$current;
-					}
-					echo "&copy"." ".$cy." Guru Computers Ltd"."\n";
-					?>
-					<img class="social-media" src="./img/facebook.png" alt="Facebook Logo">
-					<img class="social-media" src="./img/twitter.png" alt="Twitter Logo">		
-			</center>
+			
 		</footer>
 	</div>
 </body>
