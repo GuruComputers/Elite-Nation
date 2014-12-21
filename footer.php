5afd487d7212eb0d83817e50907eae633b0d7f98
Rev Neil Wakely <neil@gurucomputers.co.uk>
2014-12-21 19:23:07 +0000

    footer guru a element added

--- footer.php
+++ footer.php
@@ -11,7 +11,7 @@
 			if ($current > $start) {
 				$cy=$start."-".$current;
 			}
-			echo "&copy"." ".$cy." Guru Computers Ltd"."\n";
+			echo "&copy"." ".$cy." <a href=\"gurucomputers.co.uk\"Guru Computers Ltd</a>"."\n";
 		?>
 		<img class="social-media" src="./img/facebook.png" alt="Facebook Logo">
 		<img class="social-media" src="./img/twitter.png" alt="Twitter Logo">		
