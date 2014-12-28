d59b5423fc7d6fb7089d6a30deb05db9fbd6aaf4
Rev Dave Wakely <dave@gurucomputers.co.uk>
2014-12-28 23:29:48 +0000

    Github Debarf

--- footer.php
+++ footer.php
@@ -11,19 +11,13 @@
 			if ($current > $start) {
 				$cy=$start."-".$current;
 			}
-<<<<<<< HEAD
 
 			if(isset($login_session)){
 				echo "<marquee behavior=\"scroll\" direction=\"left\" onmouseover=\"this.stop();\" onmouseout=\"this.start();\">";
 				include('includes/viewonline.php');
 				echo "</marquee>";
 			}
-				
-=======
-		?>
-			<marquee id="players" behavior="scroll" direction="left" onmouseover="this.stop();" onmouseout="this.start();"><?php include('includes/viewonline.php'); ?></marquee>
-		<?php	
->>>>>>> FETCH_HEAD
+					
 			echo "&copy"." ".$cy." <a href=\"http://www.gurucomputers.co.uk\">Guru Computers Ltd</a>"."\n";
 		?>
 		<a href="https://www.facebook.com/elitenationgame"><img class="social-media" src="./img/facebook.png" alt="Facebook Logo"></a>
