f6ea1582530b02a8f806ba81cabaa916fe90acdc
Rev Neil Wakely <neil@gurucomputers.co.uk>
2014-12-28 20:44:14 +0000

    footer edited to include current users online

--- footer.php
+++ footer.php
@@ -14,6 +14,10 @@
 			echo "&copy"." ".$cy." <a href=\"http://www.gurucomputers.co.uk\">Guru Computers Ltd</a>"."\n";
 		?>
 		<a href="https://www.facebook.com/elitenationgame"><img class="social-media" src="./img/facebook.png" alt="Facebook Logo"></a>
-		<a href="https://twitter.com/EliteNationGame"><img class="social-media" src="./img/twitter.png" alt="Twitter Logo"></a>	
+		<a href="https://twitter.com/EliteNationGame"><img class="social-media" src="./img/twitter.png" alt="Twitter Logo"></a>
+		<?php
+			echo "    Current Player's Online : ";
+			include('includes/onlinenow.php');
+		?>
 	</center>
 </footer>
