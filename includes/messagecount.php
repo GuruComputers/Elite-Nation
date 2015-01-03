de34fd0792f61ccdbb5918eb3de985c14d8c9a7f
Rev Dave Wakely <dave@gurucomputers.co.uk>
2015-01-03 17:45:52 +0000

    Dave Debarf

--- includes/messagecount.php
+++ includes/messagecount.php
@@ -1,6 +1,6 @@
 <?php
 include('session.php');
-require("includes/variables.php");
+require("variables.php");
 
 $nsql = "SELECT * FROM pm WHERE sendto='".mysql_real_escape_string($name)."' and del='1'";            
 $nres = mysql_query($nsql) or die(mysql_error());
