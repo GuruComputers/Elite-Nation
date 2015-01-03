fd1b23eac35dbe5b29095b766ae90fdf3afb9888
Rev Dave Wakely <dave@gurucomputers.co.uk>
2015-01-03 17:34:56 +0000

    Footer Bugfix

--- includes/footer.php
+++ includes/footer.php
@@ -14,7 +14,7 @@
 
 			if(isset($login_session)){
 				echo "<marquee behavior=\"scroll\" direction=\"left\" onmouseover=\"this.stop();\" onmouseout=\"this.start();\">";
-				include('../includes/viewonline.php');
+				include('viewonline.php');
 				echo "</marquee>";
 			}
 					
@@ -24,7 +24,7 @@
 		<a href="https://twitter.com/EliteNationGame"><img class="social-media" src="./img/twitter.png" alt="Twitter Logo"></a>
 		<?php
 			echo "    Current Player's Online : ";
-			include('../includes/onlinenow.php');
+			include('onlinenow.php');
 		?>
 	</center>
 </footer>
--- includes/onlinenow.php
+++ includes/onlinenow.php
@@ -1,6 +1,6 @@
 <?php
 
-require("includes/db_config.php");
+require("db_config.php");
 
 $sql = "SELECT username FROM users WHERE DATE_SUB(NOW(), INTERVAL 5 MINUTE) <= lastactive ORDER BY id ASC"; // Searhes the database for every player showing as Last Active within the last 5 Minutes
 
