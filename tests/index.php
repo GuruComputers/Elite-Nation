9ff7ef5f41d62b11b0b8aff1ef853c1260126953
Rev Neil Wakely <neil@gurucomputers.co.uk>
2015-01-04 18:24:09 +0000

    hide test txt file

--- tests/index.php
+++ tests/index.php
@@ -1,7 +1,7 @@
 <?php
 	$files = scandir('./'); 
 	foreach ($files as $file){
-		if($file != '.' && $file != '..' && $file != 'index.php'){
+		if($file != '.' && $file != '..' && $file != 'index.php' && $file != 'test.txt'){
 			$withoutExt = preg_replace('/\\.[^.\\s]{3,4}$/', '', $file);
 			echo '<a href="'.$file.'" target="_blank">'."\n";
 			echo '<h2>'.$withoutExt.'</h2></a></li>'."\n";
