cdd5d41243c0e76f77c080703c455d5f9a4cb135
Rev Neil Wakely <neil@gurucomputers.co.uk>
2015-01-04 18:09:11 +0000

    variable names edited

--- tests/Hack Algorithm.php
+++ tests/Hack Algorithm.php
@@ -7,6 +7,7 @@ $C_D = 0;
 $R_D = 0;
 $P_D = 0;
 $D_E_D = 0;
+
 	//OTHERS
 $Hackable = 1;
 $Min_Time = 5;
@@ -15,18 +16,18 @@ $Max_Time = 300;
 $Hack_Speed = 0;
 
 //CHANGE THIS SECTION WITH IMPORTED VALUES
-$T_F = 11;
-$L_W = 1;
-$L_I = 10;
-$T_I = 1;
-$L_C = 10;
-$T_C = 1;
-$L_R = 10;
-$T_R = 1;
-$L_P = 5;
-$T_P = 1;
-$L_D = 11;
-$T_E = 1;
+$target_firewall = 11;
+$local_waterdrill = 1;
+$local_internet = 10;
+$target_internet = 1;
+$local_cpu = 10;
+$target_cpu = 1;
+$local_ram = 10;
+$target_ram = 1;
+$local_process = 5;
+$target_process = 1;
+$local_decryptor = 11;
+$target_encryptor = 1;
 
 //SCORE WEIGHTS
 $T_F_S = 0.01838668661;
@@ -43,12 +44,12 @@ $L_D_S = 0.01838668661;
 $T_E_S = 0.01838668661;
 
 //SETTING DIFFERENCES
-$F_W_D = $L_W - $T_F;
-$I_D = $L_I - $T_I;
-$C_D = $L_C - $T_C;
-$R_D = $L_R - $T_R;
-$P_D = $L_P - $T_P;
-$D_E_D = $L_D - $T_E;
+$F_W_D = $local_waterdrill - $target_firewall;
+$I_D = $local_internet - $target_internet;
+$C_D = $local_cpu - $target_cpu;
+$R_D = $local_ram - $target_ram;
+$P_D = $local_process - $target_process;
+$D_E_D = $local_decryptor - $target_encryptor;
 
 //CHECKING IF HACKABLE
 if ($F_W_D > 10 or $F_W_D < -10) {
@@ -71,18 +72,18 @@ if ($D_E_D > 10 or $P_D < -10) {
 }
 
 //CALCULATING SPEED
-$T_F_Sp = $T_F * $T_F_S * $F_W_D;
-$L_W_Sp = $L_W * $L_W_S * $F_W_D;
-$L_I_Sp = $L_I * $L_I_S * $I_D;
-$T_I_Sp = $T_I * $T_I_S * $I_D;
-$L_C_Sp = $L_C * $L_C_S * $C_D;
-$T_C_Sp = $T_C * $T_C_S * $C_D;
-$L_R_Sp = $L_R * $L_R_S * $R_D;
-$T_R_Sp = $T_R * $T_R_S * $R_D;
-$L_P_Sp = $L_P * $L_P_S * $P_D;
-$T_P_Sp = $T_P * $T_P_S * $P_D;
-$L_D_Sp = $L_D * $L_D_S * $D_E_D;
-$T_E_Sp = $T_E * $T_E_S * $D_E_D;
+$T_F_Sp = $target_firewall * $T_F_S * $F_W_D;
+$L_W_Sp = $local_waterdrill * $L_W_S * $F_W_D;
+$L_I_Sp = $local_internet * $L_I_S * $I_D;
+$T_I_Sp = $target_internet * $T_I_S * $I_D;
+$L_C_Sp = $local_cpu * $L_C_S * $C_D;
+$T_C_Sp = $target_cpu * $T_C_S * $C_D;
+$L_R_Sp = $local_ram * $L_R_S * $R_D;
+$T_R_Sp = $target_ram * $T_R_S * $R_D;
+$L_P_Sp = $local_process * $L_P_S * $P_D;
+$T_P_Sp = $target_process * $T_P_S * $P_D;
+$L_D_Sp = $local_decryptor * $L_D_S * $D_E_D;
+$T_E_Sp = $target_encryptor * $T_E_S * $D_E_D;
 
 $Hack_Speed = $Base_Time - $T_F_Sp - $L_W_Sp - $L_I_Sp - $T_I_Sp - $L_C_Sp - $T_C_Sp - $L_R_Sp - $T_R_Sp - $T_R_Sp - $L_P_SP - $T_P_Sp - $L_D_Sp - $T_E_Sp;
 
